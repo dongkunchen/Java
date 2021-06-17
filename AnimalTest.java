52a8829b7d73a93858d1577848b13a9cc3dc1e2b
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 22:12:39 +0800

    Add files via upload

---
+++ AnimalTest.java
@@ -0,0 +1,37 @@
+package com.atguigu.java;
+
+public class AnimalTest {
+	public static void main(String[] args) {
+		Animal a = new Animal();
+		a.name = "大黃";
+		a.setLegs(6);
+		
+		a.show();
+		
+		//System.out.println(a.getLegs());
+		
+	}
+
+}
+
+class Animal {
+	
+	String name;
+	int age;
+	private int legs;
+	
+	public void setLegs(int l){
+		if(l > 0 && l % 2 == 0) {
+			legs = l;
+		}else {
+			legs = 0;
+		}
+	}
+	public int getLegs(){
+		return legs;
+	}
+	public void show(){
+		System.out.println(legs);
+		
+	}
+}
