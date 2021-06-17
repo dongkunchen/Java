fab6d52ae3903b26a4e553a14b6f078557b836e2
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 22:15:10 +0800

    Add files via upload

---
+++ Kids.java
@@ -0,0 +1,28 @@
+package com.atguigu.exer;
+
+public class Kids extends ManKind {
+	private int yearsOld;
+	
+	
+	
+	public Kids() {
+	}
+
+	public Kids(int yearsOld) {
+			this.yearsOld = yearsOld;
+	}
+
+	public void printAge() {
+		System.out.println("I am " + yearsOld + "years old!");
+	}
+
+	public int getYearsOld() {
+		return yearsOld;
+	}
+
+	public void setYearsOld(int yearsOld) {
+		this.yearsOld = yearsOld;
+	}
+	
+
+}
