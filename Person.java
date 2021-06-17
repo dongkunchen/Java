e76c69ca9ac18632124d0f9db568d8167774655e
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 22:07:22 +0800

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,30 @@
+package com.atguigu.exer;
+
+public class Person {
+	
+	private int age;
+	private String name;
+	
+	public Person(String n ,int a) {
+		age = a;
+		name = n;
+	}
+
+	public void setAge(int a) {
+		if(a < 0 || a > 130) {
+			System.out.println("傳入的數據非法!");
+			return;
+		}
+		
+		age = a;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setName(String n) {
+		name = n;
+	}
+	public String getName() {
+		return name;
+	}
+}
