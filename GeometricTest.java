bc6bbd8a55fdee3037d67cb1c1a99b182b699c93
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 22:45:52 +0800

    Update GeometricTest.java

--- GeometricTest.java
+++ GeometricTest.java
@@ -1,11 +1,5 @@
 package com.atguigu.exer;
-/*
- * 
- * 定义一个测试类GeometricTest，
- * 编写equalsArea方法测试两个对象的面积是否相等（注意方法的参数类型，利用动态绑定技术），
- * 编写displayGeometricObject方法显示对象的面积（注意方法的参数类型，利用动态绑定技术）。
 
- */
 public class GeometricTest {
 
 	public static void main(String[] args) {
@@ -17,7 +11,7 @@ public class GeometricTest {
 		test.displayGeometricObject(c2);
 		
 		boolean isEquals = test.equalsArea(c1, c2);
-		System.out.println("c1 和 c2的面积是否相等：" + isEquals);
+		System.out.println("c1 和 c2 的面積是否相等：" + isEquals);
 		
 		MyRectangle rect = new MyRectangle(2.1, 3.4, "red", 2.0);
 		test.displayGeometricObject(rect);
@@ -25,10 +19,10 @@ public class GeometricTest {
 	}
 	
 	public void displayGeometricObject(GeometricObject o){//GeometricObject o = new Circle(...)
-		System.out.println("面积为：" + o.findArea());
+		System.out.println("面積為：" + o.findArea());
 	}
 	
-	//测试两个对象的面积是否相等
+	//測試兩對象面積是否相等
 	public boolean equalsArea(GeometricObject o1,GeometricObject o2){
 		return o1.findArea() == o2.findArea();
 	}
