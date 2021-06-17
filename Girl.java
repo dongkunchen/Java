bf6adeaabf29980911e2bdd1b3a007531bd2938d
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 22:10:14 +0800

    Update Girl.java

--- Girl.java
+++ Girl.java
@@ -1,4 +1,3 @@
-package com.atguigu.exer2;
 
 public class Girl {
 	private String name;
@@ -22,14 +21,7 @@ public class Girl {
 	public void marry(Boy boy) {
 		System.out.println("我想嫁給" + boy.getName());
 	}
-	/**
-	 * 
-	 * @Description 比較兩個對象的大小
-	 * @author DongKun-Chen
-	 * @date 2021年2月28日下午11:59:10
-	 * @param girl
-	 * @return 正數:當前對象大 負數:當前對象小 0:兩對象相等
-	 */
+	
 	public int compare(Girl girl) {
 		return this.age - girl.age;
 		
