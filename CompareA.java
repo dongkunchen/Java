a7cc2d2f148a7f91468116a5956170f5f2a65681
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 22:31:08 +0800

    Update CompareA.java

--- CompareA.java
+++ CompareA.java
@@ -1,17 +1,16 @@
-package com.atguigu.java8;
 
 public interface CompareA {
 	
 	public static void method1() {
-		System.out.println("CompareA:北京");
+		System.out.println("CompareA:東京");
 	}
 
 	public default void method2() {
-		System.out.println("CompareA:上海");
+		System.out.println("CompareA:南京");
 	}
 	
 	default void method3() {
-		System.out.println("CompareA:上海");
+		System.out.println("CompareA:南京");
 	}
 	
 	
