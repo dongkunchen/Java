708f6eded8974083b8a29f24f62103053a4e543b
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 18:17:37 +0800

    Add files via upload

---
+++ InstanceTest.java
@@ -0,0 +1,24 @@
+package com.atguigu.java;
+
+public class InstanceTest {
+	public static void main(String[] args) {
+		Phone p = new Phone();
+		System.out.println(p);
+		p.sendEmail();
+		p.palyGame();
+		
+	}
+
+}
+
+class Phone{
+	double price;
+	
+	public void sendEmail() {
+		System.out.println("發送郵件");
+	}
+	
+	public void palyGame() {
+		System.out.println("玩遊戲");
+	}
+}
