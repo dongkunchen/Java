b63824205e1781dea0e6c44e193411020562eb40
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 22:09:25 +0800

    Add files via upload

---
+++ BoyGirlTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.exer2;
+
+public class BoyGirlTest {
+	public static void main(String[] args) {
+		Boy boy = new Boy("羅密歐", 21);
+		boy.shout();
+		
+		Girl girl = new Girl("茱麗葉", 18);
+		girl.marry(boy);
+		
+	}
+
+}
