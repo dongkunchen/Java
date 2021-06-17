7f502b5267b5a7c836a2ac129b09d4137468026b
dongkunchen <85998522+dongkunchen@users.noreply.github.com>
2021-06-17 22:39:52 +0800

    Add files via upload

---
+++ PayrollSystem.java
@@ -0,0 +1,28 @@
+package com.atguigu.exer2;
+
+import java.util.Calendar;
+
+public class PayrollSystem {
+	public static void main(String[] args) {
+		
+		Calendar calendar = Calendar.getInstance();
+		int month = calendar.get(Calendar.MONTH);
+		//System.out.println(month);
+		
+		Employee[] emps = new Employee[2];
+		
+		emps[0] = new SalariedEmployee("馬森",1002,new MyDate(1992, 2, 28),10000);
+		emps[1] = new HourlyEmployee("潘雨生",2001,new MyDate(1991, 3, 6),60,240);
+		
+		for(int i = 0;i < emps.length;i++) {
+			System.out.println(emps[i]);
+			double salary = emps[i].earnings();
+			System.out.println("月工資為:" + salary);
+			
+			if((month+1) == emps[i].getBirthday().getMonth()) {
+				System.out.println("生日快樂!獎勵100元");
+			}
+		}
+			
+	}
+}
---
+++ SalariedEmployee.java
@@ -0,0 +1,28 @@
+package com.atguigu.exer2;
+
+public class SalariedEmployee extends Employee {
+	private double monthlySalary;
+
+	public SalariedEmployee(String name, int number, MyDate birthday, double monthlySalary) {
+		super(name, number, birthday);
+		this.monthlySalary = monthlySalary;
+		
+	}
+	
+	public double getMonthlySalary() {
+		return monthlySalary;
+	}
+
+	public void setMonthlySalary(double monthlySalary) {
+		this.monthlySalary = monthlySalary;
+	}
+
+	public double earnings() {
+		return monthlySalary;
+	}
+	
+	public String toString() {
+		return "SalariedEmployee[" + super.toString() + "]";
+	}
+
+}
